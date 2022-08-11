29c18c147d04231e71d84cf67fc55a4cf4b03398
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-08-11 17:25:16 +0200

    remove build egg-info

--- build/lib/eco_helper/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-This is ``eco_helper`` - a package to facilitate data handling and pre-processing for later work with the `EcoTyper framework <https://github.com/digitalcytometry/ecotyper>`_.
-The tools offered by this package are all accessible via a command line interface and this is the intended way to use them.
-"""
