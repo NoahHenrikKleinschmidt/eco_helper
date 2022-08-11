d0b632d2e398ba415db26f0905615e07731f31c9
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-08-11 15:18:45 +0200

    add full CLI to docs

--- build/lib/eco_helper/convert/__init__.py
+++ build/lib/eco_helper/convert/__init__.py
@@ -15,7 +15,7 @@ The tabular data formats supported are:
 
 | format | separator |
 | ------ | --------- |
-| csv    | ,         |
+| csv    |           |
 | tsv    | <tab>     |
 | txt    | <space>   |
 
@@ -26,6 +26,51 @@ Usage
 
     where <input> is the input file and <output> is the output file. The <from> and <to> options are only required if the format is not explicit from the file suffix.
     They are not case sensitive. If the <output> option is not specified, the output file will be the same as the input file with the new format.
+
+Full CLI
+========
+
+.. code-block:: bash
+
+    usage: eco_helper convert [-h] [-o OUTPUT] [-r] [--from FMT_IN] [--to FMT_OUT]
+                            [-i] [-d DATA] [-m METADATA [METADATA ...]]
+                            input
+
+    This command converts between different formats. It is able to convert tabular
+    dataformats (csv,tsv,txt) to and from mtx format. It can also extract data
+    from a SeuratObject (stored in an RDS file) and convert the data to tabular
+    formats.
+
+    positional arguments:
+    input                 Input file.
+
+    options:
+    -h, --help            show this help message and exit
+    -o OUTPUT, --output OUTPUT
+                            Output file. By default the same as the input with
+                            altered suffix.
+    -r, --recursive       Use this to mark the output as a directory rather than
+                            a target output file.
+    --from FMT_IN         The input format in case it is not evident from the
+                            input file suffix.
+    --to FMT_OUT          The output format in case it is not evident from the
+                            output file suffix.
+    -i, --index           Use this to also save the index (rownames) to tabular
+                            output files. By default the index will NOT be written
+                            to the output files. In case of SeuratObject data this
+                            option only applies to metadata tables. The extracted
+                            data will **always** have an index.
+    -d DATA, --data DATA  [Used only for Seurat-RDS] The data to extract from
+                            the SeuratObject. If not specified, by default the
+                            'counts' slot will be extracted.
+    -m METADATA [METADATA ...], --metadata METADATA [METADATA ...]
+                            [Used only for Seurat-RDS] The metadata to extract
+                            from the SeuratObject. This may be any number
+                            accessible slots or attributes of the SeuratObject. If
+                            not specified, by default a 'meta.data' attribute is
+                            tried to be extracted.
+
+
 """
 
 from .funcs import *
