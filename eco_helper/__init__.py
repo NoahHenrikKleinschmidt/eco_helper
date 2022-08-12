89ebc95a0cf96d615f6d46bf4784fc03e16ebb08
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-08-12 14:15:01 +0200

    add stuff to __init__

--- eco_helper/__init__.py
+++ eco_helper/__init__.py
@@ -3,3 +3,6 @@ This is ``eco_helper`` - a package to facilitate data handling and pre-processin
 The tools offered by this package are all accessible via a command line interface and this is the intended way to use them. 
 However, the package can be used directly as a library.
 """
+import eco_helper.core as core
+import eco_helper.format as format
+import eco_helper.normalise as normalise
