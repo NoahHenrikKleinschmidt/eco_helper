ba0a85f61a7af24a92dbcdc6a692b302c63a31d5
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-08-24 13:18:36 +0200

    move cell type, state, etc. handling classes from eco_validate

--- eco_helper/core/__init__.py
+++ eco_helper/core/__init__.py
@@ -3,3 +3,6 @@ This module defines core functions used by `eco_helper` and its submodules.
 """
 
 import eco_helper.core.terminal_funcs as terminal
+import eco_helper.core.settings as settings
+from eco_helper.core.cell_types import CellTypeCollection
+from eco_helper.core.cell_states import CellStateCollection
