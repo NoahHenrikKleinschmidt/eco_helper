dd4eff0a2dbc3f2b4cbe301e416aa4cb3f76fafa
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2022-08-25 13:46:14 +0200

    add EcotypeCollection + Ecotype

--- eco_helper/core/__init__.py
+++ eco_helper/core/__init__.py
@@ -6,3 +6,4 @@ import eco_helper.core.terminal_funcs as terminal
 import eco_helper.core.settings as settings
 from eco_helper.core.cell_types import CellTypeCollection
 from eco_helper.core.cell_states import CellStateCollection
+from eco_helper.core.ecotypes import EcotypeCollection, Ecotype
